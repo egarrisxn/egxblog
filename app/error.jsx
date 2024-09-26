f668dbe4f45af1f55861a101536238598e180c0c
EGARRISXN <egarrisxn@gmail.com>
2024-09-26 18:40:41 -0400

    minor tweaks

--- app/error.jsx
+++ app/error.jsx
@@ -2,13 +2,16 @@
 import {Button} from '../components/ui/button'
 
 export default function Error({error, reset}) {
+  console.log({error})
   return (
-    <article>
-      <h1>Oh no! Something went wrong.</h1>
-      <Button variant='outline' onClick={reset} className='bg-red-400 px-8'>
+    <div className='mx-auto flex flex-col items-center justify-center text-center'>
+      <p className='my-4 text-2xl font-semibold'>Something went wrong!</p>
+      <Button
+        onClick={reset}
+        className='rounded-lg border-2 bg-blue-500 px-8 py-2 text-white shadow-lg'
+      >
         Refresh
       </Button>
-      <pre>{error.message}</pre>
-    </article>
+    </div>
   )
 }
